a6e42846dfe8cf72273be621e31c77e91f0a3b87
DavideCruciani <cruciani240203@gmail.com>
2022-09-29 23:22:53 +0200

    more modal details

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 const addButton = document.querySelector(".add-button")
-const card = document.querySelector(".book")
+const overlay = document.querySelector(".overlay")
+const modal = document.querySelector(".modal")
 
+/* BUTTON TO OPEN THE MODAL */
 
+addButton.addEventListener("click", () => {
+  overlay.classList.add("active");
+  modal.classList.add("active");
+})
+
+/* BUTTON TO CLOSE THE MODAL */
+
+overlay.addEventListener("click", () => {
+  overlay.classList.remove("active");
+  modal.classList.remove("active");
+})
